d69122ef3a6f83dd215fe5f759ad857ff63a6974
timtikh <timtikhonov33@gmail.com>
2022-12-06 01:07:32 +0300

    Reader

--- Reversi/src/Reader.java
+++ Reversi/src/Reader.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Reader {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Reader {
+    Scanner scanner = new Scanner(System.in);
+
+    public static int readPlayersCell(Cell[] cells){
+        int cellNumber = scanner.nextInt();
+        if (cellNumber < 0 || cellNumber > 63) {
+            System.out.println("Wrong cell number!");
+            return -1;
+        }
+        for (int i = 0 ; i < cells.length; i++) {
+            if (cells[i] != null) {
+                if (cells[i].getValue().equals("0" + cellNumber) || cells[i].getValue().equals("" + cellNumber)) {
+                    return cellNumber;
+                }
+            }
+        }
+        System.out.println("Wrong cell number!");
+        return -1;
+    }
+
+
 }
