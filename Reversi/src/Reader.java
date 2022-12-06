c0dea1b5d3ce4e91ad18ebaeca40b6537937d070
timtikh <95489979+timtikh@users.noreply.github.com>
2022-12-06 18:06:33 +0300

    fiexed cell choice crash

--- Reversi/src/Reader.java
+++ Reversi/src/Reader.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 public class Reader {
     static Scanner scanner = new Scanner(System.in);
 
+    /// проверить и почистить ввод.
     public static int readPlayersCell(Cell[] cells) {
-        int cellNumber = scanner.nextInt();
-        if (cellNumber < 0 || cellNumber > 63) {
-            System.out.println("Wrong cell number!");
-            return -1;
-        }
-        for (int i = 0; i < cells.length; i++) {
-            if (cells[i] != null) {
-                if (cells[i].getValue().equals("0" + cellNumber) || cells[i].getValue().equals("" + cellNumber)) {
-                    return cellNumber;
+        boolean flag = true;
+        while (flag) {
+            System.out.println("Choose the correct cell above:");
+            int cellNumber = scanner.nextInt();
+            for (int i = 0; i < cells.length; i++) {
+                if (cells[i] != null) {
+                    if (cells[i].getValue().equals("0" + cellNumber) || cells[i].getValue().equals("" + cellNumber)) {
+                        flag = false;
+                        return cellNumber;
+                    }
                 }
             }
         }
-        System.out.println("Wrong cell number!");
-        return -1;
+        return 0;
     }
 
 }
